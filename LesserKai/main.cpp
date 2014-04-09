ea2fcf9975ff6763b943226a5441202f8903d620
takuya3211 <takuya3211@gmail.com>
2014-04-09 11:47:36 +0900

    Initial Commit

---
+++ LesserKai/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  LesserKai
+//
+//  Created by 伊藤 拓也 on 2014/04/09.
+//  Copyright (c) 2014年 伊藤 拓也. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
+
